93e7419527edd30f10365141630fd7d879df93c8
Hanna <Hanna@Macintosh-2.local>
2016-04-25 21:30:09 -0400

    site skeleton single page design working - no styling

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel = "stylesheet" type = "text/css" href="css/stylesheet.css">
+		<title>Cornell Organizational Robotics Lab</title>
+		<script src = "javascript/jquery.js"></script>
+	</head>
+
+	<body>
+		<!-- Inspiration for single-page design from https://codeplanet.io/how-to-make-a-single-page-website/ -->
+		<div id = "nav">
+			<ul>
+				<li><a href="#home">Home</a></li>
+				<li><a href="#news">News</a></li>
+				<li><a href = "#research">Research</a></li>	
+				<li><a href="#team">Team</a></li>
+				<li><a href="#contact">Contact</a></li>
+				<!-- <li><a href="#login">Login</a></li> -->
+			</ul>
+		</div>
+
+		<div id = "home">
+			Home content here.
+		</div>
+		<hr>
+		<div id = "news">
+			News content here.
+		</div>
+		<hr>
+		<div id = "research">
+			Research content here.
+		</div>
+		<hr>
+		<div id = "team">
+			Team content here.
+		</div>
+		<hr>
+		<div id = "contact">
+			Contact content here.
+		</div>
+		<hr>
+		<!-- trying to figure out smooth scrolling leave this here for now please ty -->
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		// <script src="https://github.com/kswedberg/jquery-smooth-scroll/blob/master/jquery.smooth-scroll.min.js"></script>
+		// <script>
+		// $('.smooth').on('click', function() {
+		//     $.smoothScroll({
+		//         scrollElement: $('body'),
+		//         scrollTarget: '#' + this.id
+		//     });
+		    
+		//     return false;
+		// });
+		// </script>
+		-->
+	</body>
+</html>
